67aa9528c231cbf7511d6af8d696adb812fc4882
Ander Crafter <youtubcrafter@gmail.com>
2019-12-20 22:19:35 +0100

    Change Menu

--- management/index.php
+++ management/index.php
@@ -23,11 +23,27 @@
 <div class="header">
   <a href="../index.php" class="logo">MCB Computerkabinett</a>
   <div class="header-right">
-
     <a class="items" href="../info.php">Informationen</a>
-    <a class="active" href="">Login/Meine VMs</a>
+    <?php
+    session_start();
+    if(!isset($_SESSION['userid'])) {
+      echo("<meta http-equiv = 'refresh' content = '1; url = login.php?ref=index.php' />");
+      die("</div></div><p style='color: red;'>Um auf deine VMs zugreifen zu k&oumlnnen ist ein <a href='login.php'>Login</a> erforderlich! Die Weiterleitung zum Login erfolgt in der regel automatisch</p>");
+    }
+    echo "<a class='active' href='index.php'>Meine VMs</a>";
+    echo "<a class='items' href='user.php'>";
+    echo $_SESSION['nutzername'];
+    echo "</a>";
+    echo "<a class='items' href='logout.php'>Logout</a>";
+    echo "<a class='items' href='help.php'>Hilfe</a>";
+    ?>
+    
+    
   </div>
 </div>
 </head>
 <body>
-</body>
+<?php
+echo "Hier sind deine VMs im Mediencampus, ";
+echo $_SESSION['nutzername'];
+echo "!";
